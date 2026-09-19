c8643592752525c585a411e8d94b24cf18681b7c
Joeldh67 <joel_dh67@hotmail.com>
2026-09-19 18:22:27 -0500

    Fix relative paths for CSS and JavaScript files in temples.html

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,20 +2,20 @@ const menuButton = document.querySelector("#menu-button");
 const primaryNav = document.querySelector("#primary-nav");
 const header = document.querySelector("header");
 
-// Control de apertura y cierre del menú
+// control the menu button and navigation menu
 menuButton.addEventListener("click", () => {
     primaryNav.classList.toggle("open");
     menuButton.classList.toggle("open");
     header.classList.toggle("open");
 });
 
-// Año de copyright actual
+// inputs the current year in the footer
 const currentYearSpan = document.querySelector("#currentyear");
 if (currentYearSpan) {
     currentYearSpan.textContent = new Date().getFullYear();
 }
 
-// Fecha y hora de última modificación
+// inputs the last modified date and time in the footer
 const lastModifiedParagraph = document.querySelector("#lastModified");
 if (lastModifiedParagraph) {
     lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
